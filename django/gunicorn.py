ca289fca111f4455c3fc83175c444dcbbb51cc05
Tyler Pearson <tylerdpearson@gmail.com>
2017-02-19 21:31:40 -0600

    initial commit - still a lot to be done

---
+++ django/gunicorn.py
@@ -0,0 +1,15 @@
+# gunicorn.py
+import multiprocessing
+import os
+
+if os.environ.get('MODE') == 'dev':
+    reload = True
+
+bind = '0.0.0.0:8080'
+
+timeout = 300
+#errorlog='/log/api.log'
+loglevel='debug'
+capture_output=True
+
+workers = multiprocessing.cpu_count() * 2 + 1
